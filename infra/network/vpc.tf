1f387c653a85f6f57c73ff8496fa4dfeebed4907
Erikson Lima <eriksonlima@gmail.com>
2023-04-29 12:16:19 -0300

    init

---
+++ infra/network/vpc.tf
@@ -0,0 +1,128 @@
+# Resources related to the VPC: vpc, peering, default_sg, route_tables.
+resource "aws_vpc" "main" {
+  cidr_block           = local.cidr_block
+  enable_dns_hostnames = true
+
+  tags = merge(
+    local.tags,
+    { Name = "${local.project_name}/${local.environment}" }
+  )
+}
+output "vpc_id" { value = aws_vpc.main.id }
+output "vpc_arn" { value = aws_vpc.main.arn }
+output "vpc_cidr" { value = aws_vpc.main.cidr_block }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    local.tags,
+    { Name = local.project_name }
+  )
+}
+
+resource "aws_nat_gateway" "nat_subnet_app_1a" {
+  connectivity_type = "public"
+  subnet_id = aws_subnet.private_app_1a.id
+  tags = merge(
+    local.tags,
+    {Name = "${local.projet_name}/${local.environment}/nat_subnet_app_1a"}
+  )
+}
+
+resource "aws_nat_gateway" "nat_subnet_app_1c" {
+  connectivity_type = "public"
+  subnet_id = aws_subnet.private_app_1c.id
+  tags = merge(
+    local.tags,
+    {Name = "${local.projet_name}/${local.environment}/nat_subnet_app_1c"}
+  )
+}
+
+resource "aws_nat_gateway" "nat_subnet_db_1a" {
+  connectivity_type = "public"
+  subnet_id = aws_subnet.private_db_1a.id
+  tags = merge(
+    local.tags,
+    {Name = "${local.projet_name}/${local.environment}/nat_subnet_db_1a"}
+  )
+}
+
+resource "aws_nat_gateway" "nat_subnet_db_1c" {
+  connectivity_type = "public"
+  subnet_id = aws_subnet.private_db_1c.id
+  tags = merge(
+    local.tags,
+    {Name = "${local.projet_name}/${local.environment}/nat_subnet_db_1c"}
+  )
+}
+
+resource "aws_default_route_table" "main" {
+  default_route_table_id = aws_route_table.public.id
+  tags = merge(
+    local.tags,
+    {Name = "${local.projet_name}/${local.environment}/default"}
+  )
+}
+
+resource "aws_default_security_group" "default_sg" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    local.tags,
+    { Name = "${local.project_name}/${local.environment}/default" }
+  )
+}
+
+resource "aws_route_table" "public" {
+  tags = merge(
+    local.tags,
+    { Name = "${local.project_name}/${local.environment}/public" }
+  )
+  vpc_id     = aws_vpc.main.id
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route_table" "rtb_subnet_app_private_1a" {
+  tags = merge(
+    local.tags,
+    { Name = "${local.project_name}/${local.environment}/rtb_subnet_app_private_1a" }
+  )
+  vpc_id     = aws_vpc.main.id
+  nat_gateway_id = aws_nat_gateway.nat_subnet_app_1a.id
+  depends_on = [aws_nat_gateway.nat_subnet_app_1a]
+}
+
+resource "aws_route_table" "rtb_subnet_app_private_1c" {
+  tags = merge(
+    local.tags,
+    { Name = "${local.project_name}/${local.environment}/rtb_subnet_app_private_1c" }
+  )
+  vpc_id     = aws_vpc.main.id
+  nat_gateway_id = aws_nat_gateway.nat_subnet_app_1c.id
+  depends_on = [aws_nat_gateway.nat_subnet_app_1c]
+}
+
+resource "aws_route_table" "rtb_subnet_db_private_1a" {
+  tags = merge(
+    local.tags,
+    { Name = "${local.project_name}/${local.environment}/rtb_subnet_db_private_1a" }
+  )
+  vpc_id     = aws_vpc.main.id
+  nat_gateway_id = aws_nat_gateway.nat_subnet_db_1a.id
+  depends_on = [aws_nat_gateway.nat_subnet_db_1a]
+}
+
+resource "aws_route_table" "rtb_subnet_db_private_1c" {
+  tags = merge(
+    local.tags,
+    { Name = "${local.project_name}/${local.environment}/rtb_subnet_db_private_1c" }
+  )
+  vpc_id     = aws_vpc.main.id
+  nat_gateway_id = aws_nat_gateway.nat_subnet_db_1c.id
+  depends_on = [aws_nat_gateway.nat_subnet_db_1c]
+}
+
+resource "aws_route" "igw" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0"
+}
