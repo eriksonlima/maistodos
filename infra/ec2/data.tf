38d749f2fad174c6e6a823a704c64860a58ca843
Erikson Lima <eriksonlima@gmail.com>
2023-04-29 15:45:00 -0300

    adicionando sg na ec2

--- infra/ec2/data.tf
+++ infra/ec2/data.tf
@@ -4,3 +4,10 @@ data "aws_subnet" "pub1" {
     values = ["metabase/default/public_1a"]
   }
 }
+
+data "aws_vpc" "main" {
+  filter {
+    name = "tag:Name"
+    values = ["metabase/default/main"]
+  }
+}
