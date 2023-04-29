fc45745cbaeb17a9e2a69afb5225a2d4eedd9000
Erikson Lima <eriksonlima@gmail.com>
2023-04-29 15:27:33 -0300

    adicionando sg na ec2

--- infra/ec2/ec2.tf
+++ infra/ec2/ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "maistodos_git_runner" {
   subnet_id = data.aws_subnet.pub1.id
   key_name = "erikson"
   associate_public_ip_address = true
+  security_groups = "acesso-ec2-sg"
   
   tags = merge(
     local.tags,
