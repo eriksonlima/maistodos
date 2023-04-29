fe6659c71f0e06411df91581159760a543ed1426
Erikson Lima <eriksonlima@gmail.com>
2023-04-29 13:11:23 -0300

    criando ec2 git runner

--- infra/ec2/data.tf
+++ infra/ec2/data.tf
@@ -4,10 +4,3 @@ data "aws_subnet" "pub1" {
     values = ["metabase/default/public_1a"]
   }
 }
-
-data "aws_key_pair" "ec2" {
-    filter {
-      name  = "tag:Name"
-      values = ["erikson"]
-    }
-}
